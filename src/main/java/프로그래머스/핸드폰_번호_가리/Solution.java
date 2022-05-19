4cf990861a425e196c77cad344d7fc41a7b3e923
한현규 <siregy1222@gmail.com>
2022-05-20 00:00:36 +0900

    5/20 practice: Replace String element using StringBuilder

---
+++ src/main/java/프로그래머스/핸드폰_번호_가리/Solution.java
@@ -0,0 +1,19 @@
+package 프로그래머스.핸드폰_번호_가리;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+
+        StringBuilder phone_numberSb = new StringBuilder(phone_number);
+
+        for(int i = 0; i < phone_numberSb.length()-4; i++) {
+
+            phone_numberSb.setCharAt(i,'*');
+            System.out.println(phone_numberSb.charAt(i));
+        }
+
+        answer = phone_numberSb.toString();
+
+        return answer;
+    }
+}
