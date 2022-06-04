8dce7fe289a78adc22dfcac1ef8836146668ca72
한현규 <siregy1222@gmail.com>
2022-06-04 21:24:34 +0900

    6/4 practice: combination backtracking

---
+++ src/main/java/프로그래머스/소수_만들기/Solution_Combination/Solution.java
@@ -0,0 +1,61 @@
+package 프로그래머스.소수_만들기.Solution_Combination;
+// return : 주어진 숫자 중 3개 더하여 소수가 되는 경우의 갯수
+// 1 <= nums[i] <= 1000, 중복이 없음
+// 3 <= nums.length <= 50
+import java.util.*;
+
+class Solution {
+
+    int ans = 0;
+
+    public int solution(int[] nums) {
+        int answer = -1;
+
+        // 3개를 뽑는 경우
+        boolean[] checked = new boolean[nums.length];
+
+
+        combination(nums, checked, 0, nums.length, 3);
+
+        answer = ans;
+
+        return answer;
+    }
+
+    public boolean isPrime(int num) {
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // nCr
+    // return answer
+    public void combination(int[] arr, boolean[] visited, int start, int n, int r) {
+
+        if(r == 0) {
+            int sum = 0;
+            for(int i = 0 ; i < visited.length; i++) {
+                if(visited[i]) {
+                    sum += arr[i];
+                }
+            }
+
+            if(isPrime(sum)) {
+                ans++;
+            }
+            return ;
+
+        }
+
+        for(int i = start; i < n; i++) {
+            visited[i] = true;
+            combination(arr, visited, i+1, n, r-1);
+            visited[i] = false;
+        }
+
+    }
+
+}
