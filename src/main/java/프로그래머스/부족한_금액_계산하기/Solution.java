2ede349b7c2122b654d2617a65f922957c27c7fd
한현규 <siregy1222@gmail.com>
2022-06-09 20:09:32 +0900

    6/9 practice: basis

---
+++ src/main/java/프로그래머스/부족한_금액_계산하기/Solution.java
@@ -0,0 +1,20 @@
+package 프로그래머스.부족한_금액_계산하기;
+
+// Original price: price -> Nth price: price * N
+// return: How much more money needed, when taking the ride "count"th.
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long totalPrice = 0L;
+
+
+        for(int i = 1; i <= count; i++) {
+            totalPrice += price * i;
+        }
+        if(money >= totalPrice) return 0;
+
+        answer = totalPrice - money;
+        return answer;
+    }
+}
