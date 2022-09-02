825db50672c733447a230b5f5159f9e0109ac3ab
Hg-lab <siregy1222@gmail.com>
2022-09-02 19:43:16 +0900

    9/2 pratice: back tracking, bfs

--- src/main/java/프로그래머스/Level2/카카오프렌즈_컬러링북/Solution.java
+++ src/main/java/프로그래머스/Level2/카카오프렌즈_컬러링북/Solution.java
@@ -1,2 +1,72 @@
-package 프로그래머스.Level2.카카오프렌즈_컬러링북;public class Solution {
+package 프로그래머스.Level2.카카오프렌즈_컬러링북;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Node {
+    int x;
+    int y;
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Solution {
+
+    static int[][] move = {{1,0},{0,1},{-1,0},{0,-1}};
+    static boolean[][] visited;
+    static int[][] map;
+    public int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+
+        visited = new boolean[m][n];
+        map = picture;
+
+        for(int i = 0; i < picture.length; i++) {
+            for(int j = 0; j < picture[i].length; j++) {
+                if(picture[i][j] != 0 && visited[i][j] == false) {
+                    Node startNode = new Node(i,j);
+                    int area = bfs(startNode, m, n);
+                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, area);
+                    ++numberOfArea;
+                }
+            }
+        }
+
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
+
+    static int bfs(Node startNode, int m, int n) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(startNode);
+        visited[startNode.x][startNode.y] = true;
+        int area = 1;
+        int colorNumOfStartNode = map[startNode.x][startNode.y];
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            for(int i = 0; i < move.length; i++) {
+
+                Node nextNode = new Node(node.x+move[i][0], node.y+move[i][1]);
+                if(isValidRange(nextNode.x, nextNode.y, m, n) && map[nextNode.x][nextNode.y] == colorNumOfStartNode && visited[nextNode.x][nextNode.y] == false) {
+
+                    visited[nextNode.x][nextNode.y] = true;
+                    q.offer(nextNode);
+                    ++area;
+                }
+            }
+        }
+
+
+        return area;
+    }
+
+    static boolean isValidRange(int x, int y, int m, int n) {
+        if(x >= 0 && y >= 0 && x < m && y < n) return true;
+        return false;
+    }
 }
--- src/main/java/프로그래머스/Level2/후보키/Solution.java
+++ src/main/java/프로그래머스/Level2/후보키/Solution.java
@@ -1,2 +1,79 @@
-package 프로그래머스.Level2.후보키;public class Solution {
+package 프로그래머스.Level2.후보키;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+class Solution {
+
+    String[][] relation;
+    ArrayList<Set<Integer>> candidateCols = new ArrayList<>();
+    public int solution(String[][] relation) {
+        this.relation = relation;
+
+        int answer = 0;
+        int numCols = relation[0].length;
+        int[] columnIndex = new int[numCols];
+        boolean[] visited = new boolean[numCols];
+        for(int i = 0; i < numCols; i++) {
+            columnIndex[i] = i;
+        }
+
+
+        for(int r = 1; r <= numCols; r++) {
+            columnCombination(columnIndex, visited, 0, r);
+        }
+
+
+        return answer = candidateCols.size();
+    }
+
+    public void columnCombination(int[] arr, boolean[] visited, int start, int r) {
+        if(r == 0) {
+            Set<Integer> visitedIndexes = new HashSet<>();
+            for(int i = 0; i < visited.length; i++) {
+                if(visited[i]) visitedIndexes.add(i);
+            }
+
+            for(Set<Integer> set : candidateCols) {
+                if(visitedIndexes.containsAll(set)) return;
+            }
+
+
+            if(isDuplicated(visited)) {
+                Set<Integer> set = new HashSet<>();
+                for(int i = 0; i < visited.length; i++) {
+                    if(visited[i])
+                        set.add(i);
+                }
+                candidateCols.add(set);
+            }
+        }
+
+        for(int i = start; i < arr.length; i++) {
+            visited[i] = true;
+            columnCombination(arr, visited, i+1, r-1);
+            visited[i] = false;
+        }
+
+    }
+
+    public boolean isDuplicated(boolean[] visited) {
+        Set<String> set = new HashSet<>();
+
+        for(String[] tuple : relation) {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < visited.length; i++) {
+                if(visited[i]) {
+                    sb.append(tuple[i]+"/");
+                }
+            }
+            if(set.contains(sb.toString())) {
+                return false;
+            }
+            set.add(sb.toString());
+        }
+
+        return true;
+    }
 }
