00b80d52aefd0fe6a8ce6687dc6dc8d17da5912d
Hg-lab <siregy1222@gmail.com>
2022-09-12 00:08:06 +0900

    9/12 practice: binary search

---
+++ src/main/java/프로그래머스/Level3/입국심사/Solution.java
@@ -0,0 +1,31 @@
+package 프로그래머스.Level3.입국심사;
+
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long left = times[0];
+        long right = (long)times[times.length-1] * n;
+
+        while(left <= right) {
+            long mid = (right + left) / 2;
+            long processed = 0;
+
+            for(int time : times) {
+                processed += mid / (long)time;
+            }
+
+            if(processed < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                answer = mid;
+            }
+        }
+
+
+        return answer;
+    }
+}
