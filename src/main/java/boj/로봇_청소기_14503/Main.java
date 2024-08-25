0e659db0b0607325b2992c5cd7939e26de475bd5
Hg-lab <siregy1222@gmail.com>
2024-08-25 23:02:24 +0900

    8/25 - robotCleaner-done

--- src/main/java/boj/로봇_청소기_14503/Main.java
+++ src/main/java/boj/로봇_청소기_14503/Main.java
@@ -32,24 +32,31 @@ public class Main {
         Point point = new Point(robotR, robotC, robotDir);
         int count = 0;
         while (true) {
-            if (point.isCleaned(map)) {
+            if (!point.isCleaned(map)) {
                 point.clean(map);
                 count++;
             }
 
-            if (!point.mustCleanAround()) {
-                if (point.canBackward()) {
-                    point.goBackward();
-                    continue;
+            if (point.mustCleanAround(map)) {
+                for (int i = 0; i < 4; i++) {
+                    point.rotate();
+                    if (point.mustCleanForward(map)) {
+                        point.goForward(map);
+                        break;
+                    }
                 }
-                break;
             } else {
-                point.rotate();
-                if (point.mustCleanForward()) {
-                    point.goForward();
+                if (point.canBackward(map)) {
+                    point.goBackward(map);
+                } else {
+                    break;
                 }
             }
+
+
         }
+        System.out.println(count);
+
         /*
         * 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
             현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우,
@@ -86,27 +93,102 @@ class Point {
         map[r][c] = 2;
     }
 
-    public boolean mustCleanAround() {
+    public boolean mustCleanAround(int[][] map) {
+        for (int d = 0; d < 4; d++) {
+            int nextR = DirUtil.getNextR(this.r, d);
+            int nextC = DirUtil.getNextC(this.c, d);
+            if (isInBoundary(nextR, nextC, map) && map[nextR][nextC] == 0) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private boolean isInBoundary(int r, int c, int[][] map) {
+        return r >= 0 && c >= 0 && r < map.length && c < map[r].length;
     }
 
     public void rotate() {
-
+        this.dir = DirUtil.getRotateDir(this.dir);
     }
 
-    public boolean mustCleanForward() {
+    public boolean mustCleanForward(int[][] map) {
+        int nextR = DirUtil.getNextR(this.r, this.dir);
+        int nextC = DirUtil.getNextC(this.c, this.dir);
+        if (isInBoundary(nextR, nextC, map)) {
+            return map[nextR][nextC] == 0;
+        }
         return false;
     }
 
-    public void goForward() {
-
+    public void goForward(int[][] map) {
+        int nextR = DirUtil.getNextR(this.r, this.dir);
+        int nextC = DirUtil.getNextC(this.c, this.dir);
+        if (isInBoundary(nextR, nextC, map)) {
+            this.r = nextR;
+            this.c = nextC;
+        }
     }
 
-    public void goBackward() {
-
+    public void goBackward(int[][] map) {
+        int backwardDir = DirUtil.getBackwardDir(this.dir);
+        int nextR = DirUtil.getNextR(this.r, backwardDir);
+        int nextC = DirUtil.getNextC(this.c, backwardDir);
+        if (isInBoundary(nextR, nextC, map)) {
+            this.r = nextR;
+            this.c = nextC;
+        }
     }
 
-    public boolean canBackward() {
+    public boolean canBackward(int[][] map) {
+        int backwardDir = DirUtil.getBackwardDir(this.dir);
+        int nextR = DirUtil.getNextR(this.r, backwardDir);
+        int nextC = DirUtil.getNextC(this.c, backwardDir);
+        if (isInBoundary(nextR, nextC, map)) {
+            return map[nextR][nextC] != 1;
+        }
         return false;
+    }
+}
+
+class DirUtil {
+    // 0123 북동남서
+    final static int[][] DIR_ARR = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    // 0123 북동남서 -> 서북동남
+    static int getRotateDir(int d) {
+        switch (d) {
+            case 0:
+                return 3;
+            case 1:
+                return 0;
+            case 2:
+                return 1;
+            case 3:
+                return 2;
+        }
+        return -1;
+    }
+
+    static int getNextR(int r, int dir) {
+        return r + DIR_ARR[dir][0];
+    }
+    static int getNextC(int c, int dir) {
+        return c + DIR_ARR[dir][1];
+    }
+
+    // 0123 북동남서 -> 남서북동
+    static int getBackwardDir(int dir) {
+        switch (dir) {
+            case 0:
+                return 2;
+            case 1:
+                return 3;
+            case 2:
+                return 0;
+            case 3:
+                return 1;
+        }
+        return -1;
     }
 }
