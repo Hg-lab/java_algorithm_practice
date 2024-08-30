095c6af7db84d19ab5be52dd57d264ee7fb38bff
Hg-lab <siregy1222@gmail.com>
2024-08-30 22:03:44 +0900

    8/30 - gear

--- src/main/java/boj/톱니바퀴_14891/Main.java
+++ src/main/java/boj/톱니바퀴_14891/Main.java
@@ -28,62 +28,54 @@ public class Main {
         directions = new int[timesK][2];
         for (int i = 0; i < timesK; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int startGearIdx = Integer.parseInt(st.nextToken());
+            int startGearIdx = Integer.parseInt(st.nextToken()) - 1;
             int dir = Integer.parseInt(st.nextToken());
-            rotate(gears, startGearIdx, dir);
-
-//            for (int j = 0; j < gears.length; j++) {
-//                System.out.println(j + " - " + Arrays.toString(gears[j]));
-//            }
-//            System.out.println();
-
+            directions[i][0] = startGearIdx;
+            directions[i][1] = dir;
         }
 
-
+        for (int i = 0; i < timesK; i++) {
+            rotate(gears, directions[i][0], directions[i][1], 0);
+        }
+        computeScore(gears);
         System.out.println(score);
     }
 
-    private static void rotate(int[][] gears, int gearIdx, int dir) {
+    private static void rotate(int[][] gears, int gearIdx, int dir, int leftOrRight) {
         if (!isInBoundary(gearIdx)) {
-            computeScore(gears);
             return;
+        }
+
+        int leftIdx = gearIdx - 1;
+        if (leftOrRight <= 0 && isDifferent(gearIdx, leftIdx)) {
+            rotate(gears, leftIdx, dir * (-1), -1);
+        }
+
+        int rightIdx = gearIdx + 1;
+        if (leftOrRight >= 0 && isDifferent(gearIdx, rightIdx)) {
+            rotate(gears, rightIdx, dir * (-1), 1);
         }
 
         // rotate
         int[] gear = gears[gearIdx];
-        if (dir == -1) {
+        if (dir == -1) { // 반시계
             int first = gear[0];
             for (int i = 1; i < TOOTH_NUM; i++) {
                 gear[i-1] = gear[i];
             }
-            gear[WHEEL_NUM - 1] = first;
+            gear[TOOTH_NUM - 1] = first;
         } else {
             int last = gear[TOOTH_NUM-1];
-            for (int i = TOOTH_NUM - 2; i >= 0; i--) {
-                gear[i] = gear[i+1];
+            for (int i = TOOTH_NUM - 1; i >= 1; i--) {
+                gear[i] = gear[i-1];
             }
-            gear[WHEEL_NUM - 1] = last;
-        }
-
-
-        int leftIdx = gearIdx - 1;
-        if (!isSame(gearIdx, leftIdx)) {
-            rotate(clone(gears), leftIdx, dir * (-1));
-        } else {
-//            computeScore(gears);
-        }
-
-        int rightIdx = gearIdx + 1;
-        if (!isSame(gearIdx, rightIdx)) {
-            rotate(clone(gears), rightIdx, dir * (-1));
-        } else {
-//            computeScore(gears);
+            gear[0] = last;
         }
     }
 
     private static void computeScore(int[][] gears) {
         for (int i = 0; i < WHEEL_NUM; i++) {
-            if (Main.gears[i][0] == 1) {
+            if (gears[i][0] == 1) {
                 int newScore = (int) Math.pow(2, i);
                 score += newScore;
             }
@@ -94,24 +86,15 @@ public class Main {
         return gearIdx >= 0 && gearIdx < WHEEL_NUM;
     }
 
-    private static boolean isSame(int gearIdx, int nextIdx) {
+    private static boolean isDifferent(int gearIdx, int nextIdx) {
         if (isInBoundary(gearIdx) && isInBoundary(nextIdx)) {
 
             if (gearIdx < nextIdx) {
-                return gears[gearIdx][2] == gears[nextIdx][6];
+                return gears[gearIdx][2] != gears[nextIdx][6];
             } else {
-                return gears[gearIdx][6] == gears[nextIdx][2];
+                return gears[gearIdx][6] != gears[nextIdx][2];
             }
         }
-        return false;
-    }
-
-    private static int[][] clone(int[][] gears) {
-        int[][] res = new int[WHEEL_NUM][TOOTH_NUM];
-
-        for (int i = 0; i < gears.length; i++) {
-            res[i] = gears[i].clone();
-        }
-        return res;
+        return true;
     }
 }
